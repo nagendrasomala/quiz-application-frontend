0debcb94663f66367b19adeb09a6c4aa14174705
NAGENDRABABU <nagendrababusomala@gmail.com>
2023-11-18 04:58:37 +0530

    first commit

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -4,7 +4,6 @@ import { useLocation, useParams } from 'react-router-dom';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { toast } from 'react-toastify';
 import { toast, ToastContainer } from 'react-toastify';  
 import 'react-toastify/dist/ReactToastify.css';
 
