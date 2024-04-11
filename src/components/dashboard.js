2e774d2e4d3b3836601d8051cdda2cab6bc14cb5
nagendra <nagendrababusomala@gmail.com>
2024-04-11 18:09:37 +0530

    first commit

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -117,7 +117,7 @@ const Dashboard = () => {
       </nav>
       
       <div className="xl:p-8 mt-10 p-4 flex  justify-center">
-        <div className="  h-1/2 w-11/12  flex xl:flex-col flex-row justify-center rounded-md">
+        <div className="  h-1/2 w-11/12  flex xl:flex-col  justify-center rounded-md">
           
             {participatedQuizzes.length > 0 ? (
               participatedQuizzes.map((quiz, index) => (
